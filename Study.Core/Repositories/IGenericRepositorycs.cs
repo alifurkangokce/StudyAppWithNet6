d777e2cb77043c7a0bc0eca802879d89b9547aca
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-03-22 15:36:06 +0300

    Add project files.

---
+++ Study.Core/Repositories/IGenericRepositorycs.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Study.Core.Repositories
+{
+    public interface IGenericRepositorycs<T> where T : class
+    {
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<bool> Any(Expression<Func<T, bool>> expression);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
+
